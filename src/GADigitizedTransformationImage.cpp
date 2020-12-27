40e90ec6d4c7e3b2b0022a89c41666a16e2785b1
sbreuils <breuils@nii.ac.jp>
2020-12-27 16:38:10 +0900

    Updated the transformation of pixels

--- src/GADigitizedTransformationImage.cpp
+++ src/GADigitizedTransformationImage.cpp
@@ -4,10 +4,12 @@
 void digitizedRotationImage(const std::vector<GATriplet>& pixels, std::vector<GATriplet>& transformedpixels, double angle){
 
     // define the transformation functor, here a digitized rotation
-    GAPixelTransformation gaPixelRotor(angle);
+    GADigitizedRotation digTransfo(angle);
+
+    GAPixelTransformation* gaPixelTransfo = new GADigitizedRotationCreator(angle);
 
     // could use std::transform instead
     for(unsigned int i=0 ;i<pixels.size() ; ++i ){
-        transformedpixels.push_back(gaPixelRotor(pixels[i]));
+        transformedpixels.push_back((*gaPixelTransfo)(pixels[i]));
     }
 }
