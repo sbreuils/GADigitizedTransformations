5378fbeca7e8538354700ee5f835c7ed4be4c698
sbreuils <breuils@nii.ac.jp>
2021-02-03 15:58:27 +0900

    Added digital horizontal shear

--- js/DigitizedTransformation.js
+++ js/DigitizedTransformation.js
@@ -119,7 +119,6 @@ let shearPoints_chs = function (points_i,theta) {
 
     let geometryReflectedPoints = new THREE.Geometry();
     geometryReflectedPoints.vertices = _.cloneDeep(points_i.geometry.vertices);//JSON.parse(JSON.stringify(points_i.geometry.vertices));
-    console.log(points_i.geometry.colors)
     geometryReflectedPoints.colors = _.cloneDeep(points_i.geometry.colors);
     console.log(geometryReflectedPoints.colors)
 
@@ -131,54 +130,31 @@ let shearPoints_chs = function (points_i,theta) {
     return new THREE.Points(geometryReflectedPoints,points_i.material);
 }
 
+let shearPoints_vqs = function (points_i,theta, omega) {
+    // apply a digital shear transformation to a set of points given inside the geometry of points_i
+
+    let a = Math.floor(omega*Math.sin(theta));
+    let cp = Math.floor(omega/2.0);
+    let b = omega;
+    let c = cp;
+
+    let geometryReflectedPoints = new THREE.Geometry();
+    geometryReflectedPoints.vertices = _.cloneDeep(points_i.geometry.vertices);
+    geometryReflectedPoints.colors = _.cloneDeep(points_i.geometry.colors);
+
+    for(let i=0;i<geometryReflectedPoints.vertices.length;i++){
+        let shearCoefficient = Math.floor((a*(geometryReflectedPoints.vertices[i].x)+c)/b);
+        geometryReflectedPoints.vertices[i].y = geometryReflectedPoints.vertices[i].y + shearCoefficient;
+    }
+
+    return new THREE.Points(geometryReflectedPoints,points_i.material);
+}
+
+
 export var digitalRotationFromShear = function (points_i,alph) {
     // compute the digitized rotation from the quasi shear algorithm
 
-
-    let normNormalVector1 = Math.sqrt(normalVector1[0]*normalVector1[0]+normalVector1[1]*normalVector1[1]+normalVector1[2]*normalVector1[2]+normalVector1[3]*normalVector1[3]);
-    let unitNormVector1 = [normalVector1[0]/normNormalVector1,normalVector1[1]/normNormalVector1,normalVector1[2]/normNormalVector1,normalVector1[3]/normNormalVector1];
-
-    let normNormalVector2 = Math.sqrt(normalVector2[0]*normalVector2[0]+normalVector2[1]*normalVector2[1]+normalVector2[2]*normalVector2[2]+normalVector2[3]*normalVector2[3]);
-    let unitNormVector2 = [normalVector2[0]/normNormalVector2,normalVector2[1]/normNormalVector2,normalVector2[2]/normNormalVector2,normalVector2[3]/normNormalVector2];
-
-    let unitDirectionVector1 = [unitNormVector1[1],-unitNormVector1[0],unitNormVector1[2],unitNormVector1[3]]
-    let unitDirectionVector2 = [unitNormVector2[1],-unitNormVector2[0],unitNormVector2[2],unitNormVector2[3]]
-
-    console.log("unit dir vector 1 = ",unitDirectionVector1);
-
-    let geometryReflectedPoints1 = new THREE.Geometry();
-    geometryReflectedPoints1.vertices = _.cloneDeep(points_i.geometry.vertices);//JSON.parse(JSON.stringify(points_i.geometry.vertices));
-    geometryReflectedPoints1.colors = _.cloneDeep(points_i.geometry.colors);
-
-
-    let ReflectionTransformation1 = new THREE.Matrix4();
-    ReflectionTransformation1.elements = [ 1-(2*unitDirectionVector1[0]*unitDirectionVector1[0]), -2*unitDirectionVector1[0]*unitDirectionVector1[1], -2*unitDirectionVector1[0]*unitDirectionVector1[2], -2*unitDirectionVector1[0]*unitDirectionVector1[3],
-        -2*unitDirectionVector1[0]*unitDirectionVector1[1], 1-2*(unitDirectionVector1[1]*unitDirectionVector1[1]), -2*unitDirectionVector1[1]*unitDirectionVector1[2], -2*unitDirectionVector1[1]*unitDirectionVector1[3],
-        -2*unitDirectionVector1[0]*unitDirectionVector1[2], -2*unitDirectionVector1[1]*unitDirectionVector1[2], 1-2*(unitDirectionVector1[2]*unitDirectionVector1[2]), -2*unitDirectionVector1[2]*unitDirectionVector1[3],
-        0.0, 0.0, 0.0, 1.0 ];
-
-    let ReflectionTransformation2 = new THREE.Matrix4();
-    ReflectionTransformation2.elements = [ 1-(2*unitDirectionVector2[0]*unitDirectionVector2[0]), -2*unitDirectionVector2[0]*unitDirectionVector2[1], -2*unitDirectionVector2[0]*unitDirectionVector2[2], -2*unitDirectionVector2[0]*unitDirectionVector2[3],
-        -2*unitDirectionVector2[0]*unitDirectionVector2[1], 1-2*(unitDirectionVector2[1]*unitDirectionVector2[1]), -2*unitDirectionVector2[1]*unitDirectionVector2[2], -2*unitDirectionVector2[1]*unitDirectionVector2[3],
-        -2*unitDirectionVector2[0]*unitDirectionVector2[2], -2*unitDirectionVector2[1]*unitDirectionVector2[2], 1-2*(unitDirectionVector2[2]*unitDirectionVector2[2]), -2*unitDirectionVector2[2]*unitDirectionVector2[3],
-        0.0, 0.0, 0.0, 1.0 ];
-
-    // apply the first reflection transformation
-    geometryReflectedPoints1.applyMatrix4(ReflectionTransformation1);
-    console.log("first reflected points = ",geometryReflectedPoints1.vertices);
-    // discretization of the first set of reflected points
-    discretizeGeometry(geometryReflectedPoints1);
-
-
-    // apply the first reflection transformation
-    // Second reflection
-    let geometryReflectedPoints2 = new THREE.Geometry();
-    geometryReflectedPoints2.vertices = _.cloneDeep(geometryReflectedPoints1.vertices);//JSON.parse(JSON.stringify(points_i.geometry.vertices));
-    geometryReflectedPoints2.colors = _.cloneDeep(geometryReflectedPoints1.colors);
-
-    // apply the second reflection transformation
-    geometryReflectedPoints2.applyMatrix4(ReflectionTransformation2);
-    discretizeGeometry(geometryReflectedPoints2);
+    
 
     return new THREE.Points(geometryReflectedPoints2,points_i.material);
 }
